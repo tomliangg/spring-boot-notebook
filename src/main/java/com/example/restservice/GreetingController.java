5057117be56680b695c5a26ec52cad270a05357a
Tom Liang <tomliangg@gmail.com>
2020-02-07 12:54:19 -0800

    add comments

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//  @RestController annotation marks the class as a controller where every method returns a domain object instead of a view. It is shorthand for including both @Controller and @ResponseBody.
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    // The @GetMapping annotation ensures that HTTP GET requests to /greeting are mapped to the greeting() method.
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "bao") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -3,6 +3,12 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/* @SpringBootApplication is a convenience annotation that adds all of the following:
+ * @Configuration: Tags the class as a source of bean definitions for the application context.
+ * @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings
+ * @ComponentScan: Tells Spring to look for other components, configurations, and services in the com/example package, letting it find the controllers.
+ */
 @SpringBootApplication
 public class RestServiceApplication {
 
